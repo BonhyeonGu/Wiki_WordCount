6afd64ea010ac9d98578aecf98d5e2bc363e8d3c
BonhyeonGu <bonhyeon.gu@9bon.org>
2023-03-06 10:21:41 +0900

    합치기 완성

--- 2_merge_dict.py
+++ 2_merge_dict.py
@@ -1,12 +1,17 @@
 import pickle as pic
 
 if __name__ == "__main__":
-    file_locales = ['./keyOnly0.pkl', './keyOnly1.pkl', './keyOnly2.pkl', './keyOnly3.pkl']
-    firstdict = file_locales[0]
-    with open(firstdict, 'rb') as f:
-        firstdict:dict = pic.load(f)
+    file_locales = ['./keyOnly0_ret.pkl', './keyOnly1_ret.pkl', './keyOnly2_ret.pkl', './keyOnly3_ret.pkl', './keyOnly4_ret.pkl']
+    firstDict = file_locales[0]
+    with open(firstDict, 'rb') as f:
+        firstDict:dict = pic.load(f)
+    
+    for file_locale in file_locales:
+        with open(file_locale, 'rb') as f:
+            tmpDict:dict = pic.load(f)
+        firstDict.update(tmpDict)
 
-    with open('end_ret.pkl','wb') as f:
-        pic.dump(firstdict, f)
+    with open('wordCount.pkl','wb') as f:
+        pic.dump(firstDict, f)
 
     print("end")
